9ba49e76f5c1bbc74445930cb5e94e9a18066c59
Andrea.DeStefano <enricoandrea.destefano@gmail.com>
2022-04-05 16:57:41 +0200

    Aggiunto ; mancante

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ let firstName, lastName, preferedColor;
 firstName = prompt('Qual\'è il tuo nome?');
 lastName = prompt('Qual\'è il tuo cognome?');
 preferedColor = prompt('Qual\'è il tuo colore preferito?');
-//  let outputText = ` Ciao, mi chiamo ${firstName} ${lastName} ed il mio colore preferito è ${preferedColor}!`
-let outputText = `${firstName}${lastName}${preferedColor}22`
+// let outputText = ` Ciao, mi chiamo ${firstName} ${lastName} ed il mio colore preferito è ${preferedColor}!`
+let outputText = `${firstName}${lastName}${preferedColor}22`;
 document.getElementById('output-text').innerHTML =  outputText;
+
+
+
+
+
+
+// function print() {
+//   let a=document.getElementById("floatingFirstName").value + document.getElementById("floatingLastName").value + document.getElementById("floatingColor").value;
+//   document.writeln(a);
+// }
